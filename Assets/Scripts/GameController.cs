21f29c0489cc619601187467f8e6277f856258cd
Yusuf4916 <75694415+Yusuf4916@users.noreply.github.com>
2025-04-13 18:27:47 +0300

    sa

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,6 +5,8 @@ using Akila.FPSFramework;
 using System;
 using Unity.Mathematics;
 using UnityEngine.SceneManagement;
+using System.Net.Sockets;
+using Newtonsoft.Json;
 
 public class GameController : MonoBehaviour
 {
@@ -15,6 +17,11 @@ public class GameController : MonoBehaviour
     {
         GameObject pla=Instantiate(player,spawnPos.position,quaternion.identity);
         UIManagerNew.Instance.SetPlaInfo(SocketManager.Instance.player);
+
+        SocketManager.Instance.socket.OnUnityThread("JoinRooms",rooms=>{
+        var room=JsonConvert.DeserializeObject<List<Room>>(rooms.ToString())[0];
+            SocketManager.Instance.room=room;
+        });
 
     }
 }
