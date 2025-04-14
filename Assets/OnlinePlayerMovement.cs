1aa3a23d552919c57b3ce38b407b0f5ac2c76174
Yunus Emre Demirci <yemre.1268@gmail.com>
2025-04-14 14:52:05 +0300

    online player mov eklendi

---
+++ Assets/OnlinePlayerMovement.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class OnlinePlayerMovement 
+{
+    public string userId;
+    public float speed;
+    //public Vector3 pos; 
+}
