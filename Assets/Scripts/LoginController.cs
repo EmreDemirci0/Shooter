ec4e8cdcfb5ec9d7429b650b67e7ecdf6a0b8909
Yusuf4916 <75694415+Yusuf4916@users.noreply.github.com>
2025-04-15 20:43:20 +0300

    Get Rooms Emit

--- Assets/Scripts/LoginController.cs
+++ Assets/Scripts/LoginController.cs
@@ -8,63 +8,67 @@ using UnityEngine.SceneManagement;
 
 public class LoginController : MonoBehaviour
 {
-	public TMP_InputField nameInput;
-	public JoinRoomController joinRoomPrefab;
-	public Transform container;
+    public TMP_InputField nameInput;
+    public JoinRoomController joinRoomPrefab;
+    public Transform container;
 
-	void Start()
-	{
-		Player pla = new()
-		{
-			socketID = SocketManager.Instance.socket.Id,
-			userID = SocketManager.Instance.socket.Id,
-			PlaName = nameInput.text,
-			roomID = "test"
-		};
-		SocketManager.Instance.player = pla;
-		string js = JsonUtility.ToJson(pla);
-		SocketManager.Instance.socket.Emit("SetPla", js);
+    void Start()
+    {
+        Player pla = new()
+        {
+            socketID = SocketManager.Instance.socket.Id,
+            userID = SocketManager.Instance.socket.Id,
+            PlaName = nameInput.text,
+            roomID = "test"
+        };
+        SocketManager.Instance.player = pla;
+        string js = JsonUtility.ToJson(pla);
+        SocketManager.Instance.socket.Emit("SetPla", js);
 
-		SocketManager.Instance.socket.OnUnityThread("JoinRooms", rooms =>
-		{
-			var room = JsonConvert.DeserializeObject<List<Room>>(rooms.ToString())[0];
-			SocketManager.Instance.room = room;
-			SocketManager.Instance.player.roomID = room.roomID;
-			SceneManager.LoadScene("Game");
+        SocketManager.Instance.socket.OnUnityThread("JoinRooms", rooms =>
+        {
+            var room = JsonConvert.DeserializeObject<List<Room>>(rooms.ToString())[0];
+            SocketManager.Instance.room = room;
+            SocketManager.Instance.player.roomID = room.roomID;
+            SceneManager.LoadScene("Game");
 
-		});
+        });
 
 
-		SocketManager.Instance.socket.OnUnityThread("Rooms", rooms =>
-		{
-			var room = JsonConvert.DeserializeObject<List<List<Room>>>(rooms.ToString())[0];
-			for (int i = 0; i < container.childCount; i++)
-			{
-				Destroy(container.GetChild(i).gameObject);
+        SocketManager.Instance.socket.OnUnityThread("Rooms", rooms =>
+        {
+            var room = JsonConvert.DeserializeObject<List<List<Room>>>(rooms.ToString())[0];
+            for (int i = 0; i < container.childCount; i++)
+            {
+                print(container.GetChild(i).name);
+                Destroy(container.GetChild(i).gameObject);
 
-			}
-			foreach (var item in room)
-			{
-				JoinRoomController joinRoomController= Instantiate(joinRoomPrefab,container);
-				joinRoomController.SetInfo(item);
-			}
-		
-		});
-	}
+            }
+            for (int i = 1; i < room.Count; i++)
+            {
+                JoinRoomController joinRoomController = Instantiate(joinRoomPrefab, container);
+                string st = JsonUtility.ToJson(room[i]);
 
-	public void CreateRoom()
-	{
-		if (!string.IsNullOrEmpty(nameInput.text))
-		{
+                joinRoomController.SetInfo(room[i]);
+            }
 
-			SocketManager.Instance.socket.Emit("CreateRoom", nameInput.text);
+        });
+        SocketManager.Instance.socket.Emit("GetRooms");
+    }
 
-		}
+    public void CreateRoom()
+    {
+        if (!string.IsNullOrEmpty(nameInput.text))
+        {
 
-	}
+            SocketManager.Instance.socket.Emit("CreateRoom", nameInput.text);
 
-	public void Play()
-	{
+        }
 
-	}
+    }
+
+    public void Play()
+    {
+
+    }
 }
